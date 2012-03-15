7f5177609a2c2d43ea4a1ccc09ae20d944102d0c
Jökull Sólberg Auðunsson <jokull@solberg.is>
2012-03-15 11:42:53 +0000

    Deployment

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 git commit -a -m 'Deployment' && git push
 source bin/activate
 pelican posts -s settings.py -t theme
-rsync -arP output/ web@solberg.is:/web/solberg.is
+rsync -arP output/ web@wodboard.com:/web/solberg.is
